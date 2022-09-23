174533b284aa831e81714da0193b70cd164aa4e5
Dmitry Nikishov <dmitry86nik@gmail.com>
2022-09-24 02:24:40 +0300

    impl

--- ColorSelector/ViewController.swift
+++ ColorSelector/ViewController.swift
@@ -9,11 +9,73 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    //MARK: Outlets
+    @IBOutlet weak var mixerView: UIView!
+    @IBOutlet weak var blueValueLabel: UILabel!
+    @IBOutlet weak var greenValueLabel: UILabel!
+    @IBOutlet weak var redValueLabel: UILabel!
+    @IBOutlet weak var blueSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var redSlider: UISlider!
+    
+    //MARK: Private data
+    private var redValue: Float = 0.0 {
+        didSet {
+            redValueLabel.text = String(format: "%.2f", redValue/255)
+        }
     }
 
+    private var greenValue: Float = 0.0 {
+        didSet {
+            greenValueLabel.text = String(format: "%.2f", greenValue/255)
+        }
+    }
 
+    private var blueValue: Float = 0.0 {
+        didSet {
+            blueValueLabel.text = String(format: "%.2f", blueValue/255)
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        configureViews()
+    }
+
+    //MARK: Actions
+    @IBAction func blueValueChanged() {
+        blueValue = blueSlider.value
+        updateMixerView()
+    }
+    
+    @IBAction func greenValueChanged() {
+        greenValue = greenSlider.value
+        updateMixerView()
+    }
+    
+    @IBAction func redValueChanged() {
+        redValue = redSlider.value
+        updateMixerView()
+    }
+    
+    //MARK: Private functions
+    private func updateMixerView() {
+        mixerView.backgroundColor = UIColor(
+            red: CGFloat(redValue)/255,
+            green: CGFloat(greenValue)/255,
+            blue: CGFloat(blueValue)/255,
+            alpha: 1.0
+        )
+    }
+    
+    private func configureViews() {
+        mixerView.layer.cornerRadius = 10
+        redValue = redSlider.value
+        blueValue = blueSlider.value
+        greenValue = greenSlider.value
+        
+        updateMixerView()
+    }
 }
 
